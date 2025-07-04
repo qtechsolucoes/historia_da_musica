50b9c8a263cc5a613e300ae6817804d6c39baf0e
ALESSON QUEIROZ <alessonqueiroz.dev@gmail.com>
2025-07-04 14:15:27 -0300

    new commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/historia_da_musica/", // Adicione esta linha
 })
